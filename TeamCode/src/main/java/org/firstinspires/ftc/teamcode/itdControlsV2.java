33d36aaee52df9c14cf8fa1c27b96e97a9000176
stardustArc <fuzzytomatohead@outlook.com>
2025-08-02 16:33:21 +0200

    made v2, slightly simplified bumper code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itdControlsV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itdControlsV2.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -13,10 +11,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcontroller.external.samples.BasicOmniOpMode_Linear;
 
-
 @TeleOp
-public class itdControlsOpMode extends LinearOpMode {
-    //DcMotorEx motor;
+public class itdControlsV2 extends LinearOpMode {
     public DcMotor frontLeftDrive;
     public DcMotor backLeftDrive;
     public DcMotor frontRightDrive;
@@ -25,27 +21,25 @@ public class itdControlsOpMode extends LinearOpMode {
     boolean isInvert = false;
 
 
+    //speed controls/bumper outputs
+    double frontLeftOutput;
+    double backLeftOutput;
+    double frontRightOutput;
+    double backRightOutput;
+
+    //default motor speed
+    double multiplier;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
+
         //declaring motors (ID must match DS config)
-       frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left");
-       backLeftDrive = hardwareMap.get(DcMotor.class, "back_left");
-       frontRightDrive = hardwareMap.get(DcMotor.class, "front_right");
-       backRightDrive = hardwareMap.get(DcMotor.class, "back_right");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right");
         IMU imu = hardwareMap.get(IMU.class, "imu");
-
-        //brakes to slow motor/robot down faster
-       frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        //sets drive motors to run without encoder
-       frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-       backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-       frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-       backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
 
         // GoBilda motors spin counterclockwise by default, must reverse direction
         frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -56,11 +50,25 @@ public class itdControlsOpMode extends LinearOpMode {
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
+        //sets drive motors to run without encoder
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        //brakes to slow motor/robot down faster
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
@@ -102,54 +110,43 @@ public class itdControlsOpMode extends LinearOpMode {
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-            double multiplier = 0.8;
+            frontLeftOutput = (rotY + rotX + rx) / denominator;
+            backLeftOutput = (rotY - rotX + rx) / denominator;
+            frontRightOutput = (rotY - rotX - rx) / denominator;
+            backRightOutput = (rotY + rotX - rx) / denominator;
+            //not outputting values to motor power, sending to below section for simplified controls
+
+            //(probably) simpler bumper controls, input mecanum output and output power %
+            frontLeftDrive.setPower(multiplier * frontLeftOutput);
+            backLeftDrive.setPower(multiplier * backLeftOutput);
+            frontRightDrive.setPower(multiplier * frontRightOutput);
+            backRightDrive.setPower(multiplier * frontRightOutput);
+
+            //bumper speed controls
             if (gamepad1.right_bumper) {
-                multiplier = 0.75;
+                multiplier = 1; //overdrive
                 //frontLeftDrive.setPower(0.75 * frontLeftPower);
                 //backLeftDrive.setPower(0.75 * backLeftPower);
                 //frontRightDrive.setPower(0.75 * frontRightPower);
                 //backRightDrive.setPower(0.75 * backRightPower);
             } else if (gamepad1.left_bumper) {
-                multiplier = 0.5;
+                multiplier = 0.4; //low speed
                 //frontLeftDrive.setPower(0.5 * frontLeftPower);
                 //backLeftDrive.setPower(0.5 * backLeftPower);
                 //frontRightDrive.setPower(0.5 * frontRightPower);
                 //backRightDrive.setPower(0.5 * backRightPower);
 
             } else {
-                frontLeftDrive.setPower(frontLeftPower);
-                backLeftDrive.setPower(backLeftPower);
-                if (gamepad2.dpad_up) {
-                    telemetry.addData("testing",frontLeftPower); //frontLeftPower is a placeholder lol
-                }
-                frontRightDrive.setPower(frontRightPower);
-                backRightDrive.setPower(backRightPower);
-
+                multiplier = 0.65; //standard speed
 
             }
-
-            drive(frontLeftPower, frontRightPower, backLeftPower, backRightPower, multiplier);
-
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", (frontLeftOutput * multiplier), (frontRightOutput * multiplier));
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", (backLeftOutput * multiplier), (backRightOutput * multiplier));
             telemetry.addData("Yaw:",imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
-            telemetry.addData("left bumper activated?", gamepad1.leftBumperWasPressed());
             telemetry.update();
 
 
         }
     }
-
-    private void drive(double fl, double fr, double bl, double br, double multiplier){
-        frontLeftDrive.setPower(multiplier * fl);
-        backLeftDrive.setPower(multiplier * bl);
-        frontRightDrive.setPower(multiplier * fr);
-        backRightDrive.setPower(multiplier * br);
-    }
-
 }
